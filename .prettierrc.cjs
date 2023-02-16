c68549d06eddcbd834ffabf0e964b3fd57175871
hesam-init <hesam.init@gmail.com>
2023-02-16 17:39:08 +0330

    Add types to prettier and stylelint config files

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,3 +1,5 @@
+/** @type {import('prettier').Config} */
+
 module.exports = {
   printWidth: 80,
   tabWidth: 2,
--- .stylelintrc.cjs
+++ .stylelintrc.cjs
@@ -1,3 +1,5 @@
+/** @type {import('stylelint').Config} */
+
 module.exports = {
   extends: ["stylelint-config-clean-order", "stylelint-config-prettier"]
 };
