22e4cf923714d08fa2eb5f122980491981ba9add
mojtaba-1180 <mohammadsgrdev@gmail.com>
2023-04-17 14:05:21 +0430

    [UPDATE] : update homepage

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,3 @@
-import BackButtonDemo from "@component/BackButtonDemo";
-import HapticFeedbackDemo from "@component/HapticFeedbackDemo";
-import MainButtonDemo from "@component/MainButtonDemo";
-import ShowPopupDemo from "@component/ShowPopupDemo";
 import useTelegram from "@hooks/useTelegram";
 import { Link } from "react-router-dom";
 
@@ -11,16 +7,11 @@ function Home() {
   // const userId = tgApp.initDataUnsafe.user.id;
   return (
     <div>
-      <input
-        type="file"
-        className="changeimage"
-        id="changeimage"
-        accept="image/*"
-      />
+      {/*
       <MainButtonDemo />
       <BackButtonDemo />
       <ShowPopupDemo />
-      <HapticFeedbackDemo />
+      <HapticFeedbackDemo /> */}
       <Link to="/products/add">AddProduct</Link>
     </div>
   );
