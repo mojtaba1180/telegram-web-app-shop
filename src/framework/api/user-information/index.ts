248d9ba33ddc93ade8018480ba4a4e7fba42646d
mojtaba-1180 <mohammadsgrdev@gmail.com>
2023-05-17 08:19:40 +0330

    [UPDATE] : admin order complete

--- src/framework/api/user-information/index.ts
+++ src/framework/api/user-information/index.ts
@@ -1,19 +1,20 @@
+/* eslint-disable camelcase */
 import { TypeUserInfo } from "@framework/types";
 import { useQuery } from "@tanstack/react-query";
 
 import Api from "../utils/api-config";
 
 interface QueryProps {
-  userId: string | number;
+  user_Id: string | number;
 }
 
 export const fetch = async ({ queryKey }: any) => {
-  const [_key, userId] = queryKey;
-  const { data } = await Api.get(`/users/${userId}/GetUser`);
+  const [_key, user_Id] = queryKey;
+  const { data } = await Api.get(`/users/${user_Id}/GetUser`);
   return data as TypeUserInfo;
 };
 
 // eslint-disable-next-line prettier/prettier
-const useGetUserInfo = ({ userId }: QueryProps) => useQuery<TypeUserInfo>(["user-info", userId], fetch);
+const useGetUserInfo = ({ user_Id }: QueryProps) => useQuery<TypeUserInfo>(["user-info", user_Id], fetch);
 
 export default useGetUserInfo;
