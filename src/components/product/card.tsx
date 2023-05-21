7aa6b63782f1cea17a850448c6c44e93d0e438c0
mojtaba-1180 <mohammadsgrdev@gmail.com>
2023-05-21 07:59:23 +0330

    [UPDATE] : home page product news update and connect to api

--- src/components/product/card.tsx
+++ src/components/product/card.tsx
@@ -3,9 +3,11 @@ import { Link } from "react-router-dom";
 
 interface Props {
   url: string;
+  title: string;
+  price: number;
+  quantity: number;
 }
-
-function Card({ url }: Props) {
+function Card({ url, title, price, quantity }: Props) {
   const navigate = useNavigate();
 
   const handleClick = () => {
@@ -19,19 +21,19 @@ function Card({ url }: Props) {
       <div className=" ml-auto h-20 w-full  bg-[var(--tg-theme-secondary-bg-color)]" />
       <div className="flex w-full flex-col items-start justify-between p-2">
         <div className="mb-1 ml-auto h-5 w-full select-none text-right ">
-          title
+          {title}
         </div>
-        <p className="mb-1 ml-auto h-5 w-full select-none text-right text-gray-500 ">
-          description
-        </p>
         <div className="flex w-full  flex-col gap-2">
-          <div className="flex items-center justify-between">
+          {/* <div className="flex items-center justify-between">
             <div className="rounded-xl bg-[var(--tg-theme-secondary-bg-color)] px-1 pt-1 text-sm ">
               ⭐4.3
             </div>
             <div className="select-none text-sm">غذا</div>
+          </div> */}
+          <div className="self-start text-left">
+            قیمت : {price * 1000} تومان
           </div>
-          <div className="self-end  text-right">124,000 تومان</div>
+          <div className="self-start text-left">تعداد :{quantity} عدد</div>
         </div>
       </div>
     </Link>
