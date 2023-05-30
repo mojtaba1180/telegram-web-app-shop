d027844e9972eca40372404bd7bd2bf1406e0454
mojtaba-1180 <mohammadsgrdev@gmail.com>
2023-05-31 00:28:52 +0330

    [UPDATE] : update  profile and home page

--- src/containers/boxes.tsx
+++ src/containers/boxes.tsx
@@ -1,8 +1,14 @@
+import { QueryCache } from "@tanstack/react-query";
 import { Divider } from "antd";
 import { Link } from "react-router-dom";
 
 function Boxes() {
   // eslint-disable-next-line prettier/prettier
+  const queryCache = new QueryCache();
+
+  const query = queryCache.findAll(["user-info"]);
+
+  console.log(query);
   const itemClass =
     "w-full h-20 border-2 flex border-[var(--tg-theme-button-color)] border-opacity-80 items-center justify-center rounded-lg  ";
   return (
@@ -16,10 +22,10 @@ function Boxes() {
         <Link className={itemClass} to="/profile">
           حساب کاربری
         </Link>
-        <Link className={itemClass} to="/admin">
+        {/* <Link className={itemClass} to="/admin">
           ادمین
-        </Link>
-        <Link className={itemClass} to="/cart">
+        </Link> */}
+        <Link className={`${itemClass} col-span-2 `} to="/cart">
           سبد خرید
         </Link>
       </div>
